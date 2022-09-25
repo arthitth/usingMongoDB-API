083765f7eed6d486ef2903c009515fbd1aff9c94
arthitth <the.arthit@gmail.com>
2022-09-25 13:43:49 +0700

    Add project files.

---
+++ usingMongoDB-API/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace usingMongoDB_API.Models
+{
+    public class Department
+    {
+        public ObjectId Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+    }
+}
